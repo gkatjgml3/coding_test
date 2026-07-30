63fe3af70ba6cd149401a28ae6d53907556c97c3
gkatjgml3 <jiijiijijijii@gmail.com>
2026-07-30 23:47:24 +0900

    [level 0] Title: 배열 만들기 2, Time: 33.93 ms, Memory: 106 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+
+class Solution {
+    public int[] solution(int l, int r) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int i = l; i <= r; i++) {
+            String str = String.valueOf(i);
+            boolean check = true;
+
+            for (int j = 0; j < str.length(); j++) {
+                if (str.charAt(j) != '0' && str.charAt(j) != '5') {
+                    check = false;
+                    break;
+                }
+            }
+
+            if (check) {
+                list.add(i);
+            }
+        }
+
+        if (list.size() == 0) {
+            return new int[]{-1};
+        }
+
+        int[] answer = new int[list.size()];
+
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+
+        return answer;
+    }
+}
