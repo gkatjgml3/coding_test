d2cb3bc9fb428fa3c596751675c94945345ea65d
gkatjgml3 <jiijiijijijii@gmail.com>
2026-08-03 19:55:00 +0900

    [level 0] Title: 간단한 논리 연산, Time: 0.02 ms, Memory: 84.9 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181917. 간단한 논리 연산/간단한 논리 연산.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean solution(boolean x1, boolean x2, boolean x3, boolean x4) {
+        boolean first = x1 || x2;
+        boolean second = x3 || x4;
+
+        return first && second;
+    }
+}
